88a10177da496f38a30288cec932ab7f143f89c3
RichardPcLinux <javierbaron6@gmail.com>
2025-03-16 16:00:47 -0500

    Ajustes

--- src/mod/user/final/auth/dto/create-auth.dto.ts
+++ src/mod/user/final/auth/dto/create-auth.dto.ts
@@ -1,9 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString } from "class-validator";
 export class CreateAuthuserDto {
+
+    @ApiProperty({
+        description: 'Username login.',
+        type: 'string',
+        example: 'correo@example.com'
+    })
     @IsString()
     // @Transform(({value}) => value.trim())
     readonly username;
 
+    @ApiProperty({
+        description: 'Password.',
+        type: 'string',
+        example: '*********'
+    })
     @IsString()
     // @Transform(({value}) => value.trim())
     readonly pass;
--- src/mod/user/final/auth/dto/token.dto.ts
+++ src/mod/user/final/auth/dto/token.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class TokenDto {
+    @ApiProperty({
+        description: 'Acces token',
+        type: 'string',
+        example: 'eyJhbGciOiJIUzI1NiI....'
+    })
     @IsString()
     // @Transform(({value}) => value.trim())
     readonly token;
